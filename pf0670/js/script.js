0bc6a3c714bad491d33cd9fca8ec67716bbdd815
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-08-03 08:55:37 -0300

    Declara arrays e itera com loop.

---
+++ pf0670/js/script.js
@@ -0,0 +1,34 @@
+// console.log("Hello World!");
+
+// //Declarando um array de nomes de pessoas
+// let nomes = ["Diego", "Gabriel", "Lucas"];
+// //Impriindo o array
+// console.log(nomes);
+
+// //Imprimindo o primeiro nome do array
+// console.log(nomes[0]);
+
+// //Quebrando linha no console do navegador com \n
+// console.log("Quebrando linha \n");
+// console.log("\n");
+// //Imprimindo o array usando um loop for
+// for (let i = 0; i < nomes.length; i++) {
+    
+//     if (nomes[i] == "Gabriel") {
+//         break;
+//     } else {
+//         console.log(nomes[i]);
+//     }
+
+// }
+// console.log("\n");
+// //Imprimindo o array usando um loop forEch
+// nomes.forEach(function (nome) {
+//     if (nome == "Gabriel") {
+//         return true;
+//     } else {
+//         console.log(nome);
+//     }
+// });
+
+
