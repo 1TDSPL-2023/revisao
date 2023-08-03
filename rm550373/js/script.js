7ce7c8d0ba2a1be0355d6f9a75537361c5c53e1e
leoleoyuuki <rm550373@fiap.com.br>
2023-08-03 08:54:28 -0300

    Declara arrays e itera com loop

---
+++ rm550373/js/script.js
@@ -0,0 +1,37 @@
+// console.log("Hello World")
+
+//Declarando nomes de pessoas numa array
+
+// let nomes = ["Diego", "Gabriel", "Lucas"];
+
+// //imprimindo o array
+
+// console.log(nomes[0]);
+
+// //quebrando linhas no console com \n
+// console.log("Quebrando linha \n");
+// console.log("\n");
+
+
+// // imprimindo array usando loop for
+// for (let i = 0 ; i < nomes.length; i++){
+//     if (nomes[i] == "Gabriel"){
+//         break;
+//     }
+//     else{
+//         console.log(nomes[i]);
+//     }
+
+// }
+
+
+// // imprimindo array usando loop forEach
+// nomes.forEach(function (nome){
+//     if (nome == "Gabriel"){
+//         return true;
+//     }
+//     else{
+//         console.log(nome);
+//     }
+//     console.log(nome);
+// })
