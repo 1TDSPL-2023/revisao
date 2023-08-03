81ac967091aa6e941c77512c7555eea9f01ba15b
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-08-03 09:13:24 -0300

    Array de objetos com iteração e params.

--- pf0670/js/script.js
+++ pf0670/js/script.js
@@ -32,3 +32,70 @@
 // });
 
 
+//Declaração de um array objetos alunos.
+let alunos = [
+    { nome: "Diego", idade: 23, peso: 80.5 },
+    { nome: "Gabriel", idade: 15, peso: 60.5 },
+    { nome: "Lucas", idade: 30, peso: 90.5 }
+];
+
+console.log("\n");
+
+//Imprimindo o array de objetos
+console.log(alunos);
+
+console.log("\n");
+
+//Imprimindo o primeiro objeto do array
+console.log(alunos[0]);
+console.log("\n");
+//Impriindo o nome do primeiro objeto do array
+console.log(alunos[0].nome)
+console.log("\n");
+//Imprimindo o nome, idade e peso do primeiro objeto de todos os objetos do array
+console.log(alunos[0].nome, alunos[0].idade, alunos[0].peso);
+console.log(alunos[1].nome, alunos[1].idade, alunos[1].peso);
+console.log(alunos[2].nome, alunos[2].idade, alunos[2].peso);
+
+console.log("\n");
+
+//Imprimindo o nome, idade e peso de todos os objetos do array usando o valor da propriedade como String
+let propriedade = "nome";
+console.log(alunos[0][propriedade], alunos[0]["idade"], alunos[0]["peso"]);
+console.log(alunos[1][propriedade], alunos[1]["idade"], alunos[1]["peso"]);
+console.log(alunos[2][propriedade], alunos[2]["idade"], alunos[2]["peso"]);
+
+console.log("\n");
+
+//Imprimindo o nome, idade e peso de todos os objetos do array usando loop for
+for (let i = 0; i < alunos.length; i++) {
+    console.log(alunos[i].nome, alunos[i].idade, alunos[i].peso);
+} //For
+
+console.log("\n");
+
+//Imprimindo o nome, idade e peso de todos os objetos do array usando loop forEach
+alunos.forEach(function (aluno) {
+    console.log(aluno.nome, aluno.idade, aluno.peso);
+});//Função anônima
+
+console.log("\n");
+
+//Imprimindo o nome, idade e peso de todos os objetos do array usando loop forEach
+alunos.forEach(aluno => {
+    console.log(aluno.nome, aluno.idade, aluno.peso);
+} );//Arrow function
+
+console.log("\n");
+
+//Imprimindo o nome, idade e peso de todos os objetos do array usando loop for of
+for (let aluno of alunos) {
+    console.log(aluno.nome, aluno.idade, aluno.peso);
+}//For of
+
+console.log("\n");
+
+//Imprimindo o nome, idade e peso de todos os objetos do array usando loop for in
+for (let i in alunos) {
+    console.log(alunos[i].nome, alunos[i].idade, alunos[i].peso);
+} //For in
