03c380e6dcac8d7869eecc7674a5f344ed5bf56b
DouglasAraujo0 <Rm552008@fiap.com.br>
2023-08-03 08:57:50 -0300

    revisando sobre js.

---
+++ pf0670/js/script.js
@@ -0,0 +1,24 @@
+// CTRL + SHIFT + I = console do navegador
+
+
+//Declarando um array de nomes de pessoas 
+//let nomes = ['Diego','Gabriel','Lucas'];
+
+//Imprimindo o array
+//console.log(nomes);
+
+//Imprimindo o primeiro nome do array
+//console.log(nomes[0]);
+
+//Quebrar linha no console do navegador
+//console.log("\n")
+
+//Imprimindo o array usando um loop for
+//for (let i = 0; i < nomes.length; i++) {
+//    if (nomes[i] == "Gabriel") {
+//        break;
+//   } else {
+//        console.log(nomes[i]);
+//   }
+    
+
