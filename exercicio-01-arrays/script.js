4c6aff75bc45356d81057fa503484c4194c1703c
aaaaaaaaana <rm551401@fiap.com.br>
2023-08-08 09:40:20 -0300

    formulario

--- exercicio-01-arrays/script.js
+++ exercicio-01-arrays/script.js
@@ -3,9 +3,43 @@ let listaTarefasArray = []
 
 const botaoAdicionarTarefa = document.getElementById("btnAddTarefa")
 
-botaoAdicionarTarefa.addEventListener("click", function(){} );
+botaoAdicionarTarefa.addEventListener("click", (evento)=>{
+    
+    evento.preventDefault();
+    
+    const inputTarefa = document.getElementById("idTarefa");
+
+    listaTarefasArray.push(inputTarefa.value);
+
+    let li = document.createElement("li");
+    li.textContent = inputTarefa.value;
+
+    const listaTarefasUL = document.getElementById("lista-tarefas");
+    listaTarefasUL.appendChild(li);
+
+    let botaoExcluir = document.createElement("button");
+    botaoExcluir.textContent = " x ";
+
+    li.appendChild(botaoExcluir);
+
+    botaoExcluir.addEventListener("click", (evento)=>{
+
+        listaTarefasArray.forEach(tarefa=>{
+            
+            if(evento.parentNode.textContent.split(" ")[0] == tarefa){
+                listaTarefasArray.splice(listaTarefasArray.indexOf(tarefa),1)
+            }
+        })
+
+        evento.target.parentNode.remove();
+        console.log(listaTarefasArray);
+
+    })
 
 
+    inputTarefa.value = "";
+    
+});
 
 
 
