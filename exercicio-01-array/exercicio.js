eb2bf01bb48eb4b0167ed735bffcdbb3566e71cb
rafaellabastos1 <rm552425@fiap.com.br>
2023-08-09 19:28:37 -0300

    exclui o que tinha feito anteriormente pois estava errado

--- exercicio-01-array/exercicio.js
+++ exercicio-01-array/exercicio.js
@@ -1,14 +1,3 @@
-// function adicionar() {
-//     let adicionarTarefa = document.getElementById("adicionarTarefa").value;
-//     let list = document.getElementById("listaTarefas");
-//     const novoItem = document.createElement("li");
-
-//     novoItem.appendChild(document.createTextNode(adicionarTarefa));
-//     list.appendChild(novoItem);
-//     document.getElementById("adicionarTarefa").value = "";
-// }
-
-
 //Declarar a lista de tarefas vazia
 let listaTarefasArray = [];
 
